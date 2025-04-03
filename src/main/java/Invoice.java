207d9978adb37d43182b864c77099d8ea98fd2aa
James Glover <3057283g@student.gla.ac.uk>
2025-04-03 12:14:18 +0100

    Changed public attributes to private. Provided a deep copy of the array of performances. Made the tests would with my Java version

--- src/main/java/Invoice.java
+++ src/main/java/Invoice.java
@@ -1,12 +1,31 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Invoice {
 
-    public String customer;
-    public List<Performance> performances;
+    // Similar issue to the Play Class.
+    // We should not expose the internal data of the Invoice class.
+    // This is an example of X coupling
+    // This coupling is bad because ...
+    private String customer;
+    private List<Performance> performances;
 
     public Invoice(String customer, List<Performance> performances) {
         this.customer = customer;
         this.performances = performances;
+    }
+
+    public String getCustomer() {
+        return this.customer;
+    }
+
+    public List<Performance> getPerformances() {
+        List<Performance> copyPerformances = new ArrayList<>();
+        for (Performance performance: this.performances) {
+            Performance copyPerformance = new Performance(performance.getPlayID(), performance.getAudience());
+            copyPerformances.add(copyPerformance);
+        }
+
+        return copyPerformances;
     }
 }
--- src/main/java/Play.java
+++ src/main/java/Play.java
@@ -1,10 +1,21 @@
 public class Play {
 
-    public String name;
-    public String type;
+    // The attributes below were initially public.
+    // This is an example of data coupling/ global variables.
+    // This is bad because ...
+    private String name;
+    private String type;
 
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getType() {
+        return this.type;
     }
 }
