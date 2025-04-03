33590087a4b2ca2df1592b505ed62678b31de493
James Glover <3057283g@student.gla.ac.uk>
2025-04-03 12:23:36 +0100

    Added comments, explaining previous refactoring decisions for the public attributes and performances deep copy

--- src/main/java/Invoice.java
+++ src/main/java/Invoice.java
@@ -3,10 +3,14 @@ import java.util.List;
 
 public class Invoice {
 
-    // Similar issue to the Play Class.
-    // We should not expose the internal data of the Invoice class.
-    // This is an example of X coupling
-    // This coupling is bad because ...
+    // By having public attributes, other classes are relying on the internal details of
+    // the class which may change in future. Refactored to use getters and setters.
+
+    // Also, I changed performances to provide a deep copy of the performances array.
+    // This is to ensure that when we provide a getter to the performances of the array.
+    // performances List cannot be changed. Neither instances of Performance objects in the list.
+    // This refactoring is an example of Safe classes I think. Not sure though. I don't think this is an example of
+    // coupling or cohesion though.
     private String customer;
     private List<Performance> performances;
 
@@ -25,7 +29,6 @@ public class Invoice {
             Performance copyPerformance = new Performance(performance.getPlayID(), performance.getAudience());
             copyPerformances.add(copyPerformance);
         }
-
         return copyPerformances;
     }
 }
--- src/main/java/Play.java
+++ src/main/java/Play.java
@@ -2,7 +2,13 @@ public class Play {
 
     // The attributes below were initially public.
     // This is an example of data coupling/ global variables.
-    // This is bad because ...
+    // This is bad because ... Very difficult to track changes and debug
+
+    // Actually on second thoughts this seems more like content coupling.
+    // By having public attributes, other classes are relying on the internal details of
+    // the class which may change in future. Refactored to use getters and setters.
+
+    // Is this now interface coupling
     private String name;
     private String type;
 
